dd2f345f9fe5c1c07a14bae052744c4905159be3
vish2445 <166433134+vish2445@users.noreply.github.com>
2024-11-19 12:38:37 +0530

    Add files via upload
    Initial commit changes

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Home from './Screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+
+} from "react-router-dom";
+import Login from './Screens/Login';
+
+function App() {
+  return (
+    <Router>
+   <div>
+      <Routes>
+        <Route exact path="/" element ={<Home/>}/>
+        <Route exact path="/Login" element ={<Login/>}/>
+      </Routes>
+
+   </div>
+   </Router>
+  );
+}
+
+export default App;
